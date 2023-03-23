a0a5248c0e5bf9d05cca32a3d7c8ece7fffd0f5e
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-23 08:09:45 +0530

    Add files via upload

---
+++ Data Structure/Maxi.java
@@ -0,0 +1,18 @@
+package Threading;
+
+public class Maxi {
+
+	class Solution {
+	    public int maximumWealth(int[][] accounts) {
+	        int maxi = Integer.MIN_VALUE;
+	        for(int i = 0; i< accounts.length; i++){
+	          int sum = 0;
+	          for(int j = 0; j< accounts[i].length; j++){
+	              sum += accounts[i][j];
+	          }
+	          maxi = Math.max(maxi,sum);
+	        }
+	        return maxi;
+	    }
+	}
+}
