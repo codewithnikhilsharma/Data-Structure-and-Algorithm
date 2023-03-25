80863c9dccd58463e0e2a2e9b29bedfec82916a8
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-25 10:46:25 +0530

    Create Roman.java

---
+++ Data Structure/Roman.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = getValue(s.charAt(i));
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+            prevValue = currValue;
+        }
+        return result;
+    }
+    
+    private int getValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
